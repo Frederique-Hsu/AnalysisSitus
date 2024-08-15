ba2f4cdbf9ab51cb8ad23e2d1e8bff9ce7f76424
Quaoar-machine <sergey.slyadnev@gmail.com>
2024-08-15 12:47:24 +0300

    #327: add another test case for Gordon.

---
+++ scripts/re/build-gordon_18.tcl
@@ -0,0 +1,17 @@
+# Set working variables.
+set datafile cad/gordon/test-rectangle.brep
+set refErr   0.1
+
+# Read input geometry.
+set datadir $env(ASI_TEST_DATA)
+clear; load-part $datadir/$datafile; fit
+fit
+
+# Build Gordon surface.
+set maxErr [build-gordon -p 1 3 -g 2 4]
+
+puts "Max error: $maxErr"
+
+if { $maxErr > $refErr } {
+  return -code error "Max approximation error $maxErr exceeds the max allowed (reference) error $refErr."
+}
