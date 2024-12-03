914591744474d7a8a77c9c7d7d648c6145c0099a
andrey <andrey@andrey>
2024-12-03 09:35:22 +0300

    Try disabling distance evaluation in `asiAlgo_ClassifyPointSolid` for the sake of better performance. (#342)

---
+++ scripts/modeling/classify-point-solid-6.tcl
@@ -0,0 +1,13 @@
+clear
+set datafile public/cad/ANC101.stp
+
+# Read input geometry.
+set datadir $env(ASI_TEST_DATA)
+clear
+load-step $datadir/$datafile
+
+test-build-aabb-points "boxPoints" 100 100 100
+
+test-classify-point-solid "boxPoints" "boxPointsOut" 1 0
+
+test-number-of-points "boxPointsOut" 488334
