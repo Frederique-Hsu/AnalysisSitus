50876502ee6033274328af953d05c46fecd42501
Quaoar <sergey.slyadnev@gmail.com>
2025-05-06 15:15:48 +0300

    Fix #322: Allow setting custom rapidjson dir by getting rid of the `FORCE` mode in cmake.

--- cmake/asitus_Rapidjson.cmake
+++ cmake/asitus_Rapidjson.cmake
@@ -10,6 +10,6 @@ add_definitions (-DUSE_RAPIDJSON)
 
 ASITUS_FIND_PRODUCT_DIR ("${3RDPARTY_DIR}" "rapidjson" RAPIDJSON_DIR)
 
-set (3RDPARTY_rapidjson_DIR "${3RDPARTY_DIR}/${RAPIDJSON_DIR}/include" CACHE PATH "The directory containing Rapidjson." FORCE)
+set (3RDPARTY_rapidjson_DIR "${3RDPARTY_DIR}/${RAPIDJSON_DIR}/include" CACHE PATH "The directory containing Rapidjson.")
 
 message (STATUS "... Rapidjson dir: ${3RDPARTY_rapidjson_DIR}")
